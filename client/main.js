d26e2a812a3d20f5d254d252d82f29c511e38dc0
Genroa <genroa@hotmail.fr>
2016-10-12 19:01:41 +0200

    Added test SVG
    SVG with example CSS, and adapting height

---
+++ client/main.js
@@ -0,0 +1,70 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import '../lib/shared.js';
+import './main.html';
+
+Template.hello.onCreated(function gridOnCreated() {
+	this.inputNumber = new ReactiveVar(7);
+	this.outputNumber = new ReactiveVar(20);
+
+	this.gridLinks = new ReactiveDict('gridLinks');
+	for(i=0; i < this.inputNumber.get(); i++){
+		this.gridLinks.set(i.toString(), Array.apply(null, Array(this.outputNumber.get())).map(Number.prototype.valueOf,0));
+	}
+});
+
+Template.hello.helpers({
+	getGridConnection(input) {
+		return Template.instance().gridLinks.get(input);
+	},
+
+	getInputNumber(){
+		return Template.instance().inputNumber.get();
+	},
+
+	getOutputNumber(){
+		return Template.instance().outputNumber.get();
+	},
+
+	inputsRange(){
+		return [...Array(Template.instance().inputNumber.get()).keys()];
+	},
+
+	revertOutputsRange(){
+		return [...Array(Template.instance().outputNumber.get()).keys()].reverse();
+	},
+
+	columnPosition(index){
+		return (1000/Template.instance().inputNumber.get())*index+20;
+	},
+
+	columnHeight(outputIndex){
+		return Math.max(100,(1000/Template.instance().outputNumber.get()))*(outputIndex+1);
+	},
+
+	lineLength(inputIndex){
+		return 1000-(1000/Template.instance().inputNumber.get())*(inputIndex);
+	},
+
+	gridThickness(){
+		return 9;
+	},
+
+	defineSvgHeight(){
+		var minimumLineHeight = Math.max(100,(1000/Template.instance().outputNumber.get()));
+		var outputNumber = Template.instance().outputNumber.get();
+		return Math.max(1050, minimumLineHeight*outputNumber+50);
+	},
+
+	inputs(){
+		var dic = [];
+		var inputNumber = Template.instance().inputNumber.get();
+		var gridLinks = Template.instance().gridLinks;
+		for(i=0; i < inputNumber; i++){
+			dic.push(gridLinks.get(i.toString()));
+		}
+		//console.log(JSON.stringify(dic));
+		return dic;
+	}
+});
